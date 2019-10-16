5d23d6d2db58eadeeb4df0eb752c4ceb637da228
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2019-10-16 16:10:31 -0400

    ##

---
+++ 2012/3D Printer Software/FirstExample/3D Printer OS.cpp
@@ -0,0 +1,408 @@
+﻿
+//***************************************************************************
+// HG-22155-Assignment2.cpp by Galal Hassan (C) 2019 All Rights Reserved.
+//
+// Assignment 2 submission.
+//
+// Description:
+//   Click run to see the results.
+//***************************************************************************
+using namespace std;
+#include <iostream>
+#include "stdlib.h"
+#include "time.h"
+#include "vgl.h"
+#include "LoadShaders.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+
+#define X_AXIS glm::vec3(1, 0, 0)
+#define Y_AXIS glm::vec3(0, 1, 0)
+#define Z_AXIS glm::vec3(0, 0, 1)
+#define XY_AXIS glm::vec3(1, 1, 0)
+#define YZ_AXIS glm::vec3(0, 1, 1)
+#define ZX_AXIS glm::vec3(1, 0, 1)
+
+enum VAO_IDs { Triangles, NumVAOs };
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0 };
+
+
+// Horizontal and vertical ortho offsets.
+float posX, posY, posZ = 10.0f, scrollSpd = 0.25f;
+float rotAngle = 0.0f;
+
+
+int numCube = 2;
+
+glm::mat4 mvp;
+glm::mat4 view;
+glm::mat4 projection;
+GLuint gVAO1;
+
+GLuint gVAO;		// Vertex Array Object
+GLuint ibo;			// Index Buffer Object
+GLuint points_vbo;	// Vertex Buffer Object (Points)
+GLuint colors_vbo;	// Vertex Buffer Object (Colors)
+
+
+GLuint Hibo;
+GLuint Hcolors_vbo;	// Vertex Buffer Object (Colors)
+GLuint Hpoints_vbo;	// Vertex Buffer Object (Points)
+
+GLuint modelID;
+
+void init(void)
+{
+
+
+	//Specifying the name of vertex and fragment shaders.
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	//Loading and compiling shaders
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);	//My Pipeline is set up
+
+
+	modelID = glGetUniformLocation(program, "mvp");
+
+	// in world coordinates
+	projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f);
+
+	// Camera matrix
+	view = glm::lookAt(
+		glm::vec3(0, 0, posZ),		// Camera pos in World Space
+		glm::vec3(0, 0, 0),		// and looks at the origin
+		glm::vec3(0, -1, 0)		// Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+
+	
+	// Hex indicies
+	GLshort H_indices[] = {
+		// bot
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 4,
+		0, 4, 5,
+		0, 5, 6,
+		0, 6, 1,
+
+		// top 
+		7, 1, 2,
+		7, 2, 3,
+		7, 3, 4,
+		7, 4, 5,
+		7, 5, 6,
+		7, 6, 1
+
+	};
+
+	GLfloat H_vertices[] = {
+
+		// bot
+		-0.5f, -1.0f,   1.0f,     // left top 1
+		0.5f,  -1.0f,    1.0f,   // right top  2
+		1.0f,  -1.0f,     0.0f,  // right 3
+		0.5f,  -1.0f,   -1.0f,    // right bottom (notice sign) 4
+		-0.5f, -1.0f,     -1.0f,  // left bottom 5
+		-1.0f,   -1.0f,    0.0f,   // left 6
+
+		// top
+		-0.5f, 1.0f,   1.0f,     // left top 1
+		0.5f,  1.0f,    1.0f,   // right top  2
+		1.0f,  1.0f,     0.0f,  // right 3
+		0.5f,  1.0f,   -1.0f,    // right bottom (notice sign) 4
+		-0.5f, 1.0f,     -1.0f,  // left bottom 5
+		-1.0f, 1.0f,    0.0f,   // left 6
+
+
+	};
+
+	GLfloat H_colors[] = {
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+
+	};
+
+
+
+
+
+
+	// Hex indicies
+	GLshort Hex_indices[] = {
+		// bot
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 4,
+		0, 4, 5,
+		0, 5, 6,
+		0, 6, 1,
+
+		// top 
+		7, 1, 2,
+		7, 2, 3,
+		7, 3, 4,
+		7, 4, 5,
+		7, 5, 6,
+		7, 6, 1
+
+	};
+
+	// Hex verticies
+	GLfloat Hex_vertices[] = {
+		
+		0.0f,   3.0f, 0.0f,    // bot center 0
+
+		// bot
+		-0.5f, -1.0f,   1.0f,     // left top 1
+		0.5f,  -1.0f,    1.0f,   // right top  2
+		1.0f,  -1.0f,     0.0f,  // right 3
+		0.5f,  -1.0f,   -1.0f,    // right bottom (notice sign) 4
+		-0.5f, -1.0f,     -1.0f,  // left bottom 5
+		-1.0f,   -1.0f,    0.0f,   // left 6
+
+
+		0.0f,   -3.0f, 0.0f,    // top center 7
+
+
+
+	};
+
+	// Hex colors
+	GLfloat Hex_colors[] = {
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+		0.0f, 0.4f, 0.0f,
+		0.0f, 0.8f, 0.0f,
+		0.0f, 0.6f, 0.0f,
+		0.0f, 0.4f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+	};
+
+	// generate and bind Vertex Array Object
+	gVAO = 0;
+	glGenVertexArrays(1, &gVAO);
+	glBindVertexArray(gVAO);
+
+	// generate and bind Index Buffer Object
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Hex_indices), Hex_indices, GL_STATIC_DRAW);
+
+	// generate and bind points Vertex Buffer Object(VBO)
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Hex_vertices), Hex_vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+
+	// generate and bind colors Vertex Buffer Object(VBO)
+	glGenBuffers(1, &colors_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Hex_colors), Hex_colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(1);
+
+
+
+	
+	
+	
+	gVAO1 = 0;
+	glGenVertexArrays(1, &gVAO1);
+	glBindVertexArray(gVAO1);
+
+	// generate and bind Index Buffer Object
+	glGenBuffers(1, &Hibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Hibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(H_indices), H_indices, GL_STATIC_DRAW);
+
+	// generate and bind points Vertex Buffer Object(VBO)
+	glGenBuffers(1, &Hpoints_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, Hpoints_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(H_vertices), H_vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+
+	// generate and bind colors Vertex Buffer Object(VBO)
+	glGenBuffers(1, &Hcolors_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, Hcolors_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(H_colors), H_colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(1);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+
+}
+
+
+void transformObject2(glm::vec3 scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, scale);
+	mvp = projection * view * Model;
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &mvp[0][0]);
+}
+
+void transformObject(float scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, glm::vec3(scale));
+	mvp = projection * view * Model;
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &mvp[0][0]);
+}
+
+
+
+//---------------------------------------------------------------------
+//
+// display
+//
+float pos = 0.0f;
+bool flag = true;
+void
+display(void)
+{
+	view = glm::lookAt(
+		glm::vec3(posX, posY, posZ),		// Camera pos in World Space
+		glm::vec3(posX, posY, 0),		// and looks at the origin
+		glm::vec3(0, 1, 0)		// Head is up (set to 0,-1,0 to look upside-down)
+	);
+	// Clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Background Color
+	glClearColor(0.0f, 0.5f, 0.5f, 0.0f);
+
+	// Set Camera Projection
+	projection = glm::perspective(30.0f, (GLfloat)1.0f, 1.0f, 50.0f);
+
+
+
+
+
+	// Second cube
+	glBindVertexArray(gVAO);
+
+
+	if (flag)
+	{
+		pos+=0.01f;
+		if (pos >= 1.0f)
+		{
+			flag = false;
+		}
+	}
+	else 
+	{
+		pos-=0.01f;
+		if (pos <= -1.0f)
+		{
+			flag = true;
+		}
+	}
+
+	// Set cube transforamtion
+	transformObject2(glm::vec3(1.0f, 1.0f, 1.0f), Y_AXIS, rotAngle += 1.0f, glm::vec3(0.0f, pos, 0.0f));
+	// Draw cube
+	glDrawElements(GL_TRIANGLES, 48, GL_UNSIGNED_SHORT, 0);
+
+
+	// Bottom hex
+	glBindVertexArray(gVAO1);
+	transformObject2(glm::vec3(4.0f, 4.0f, 4.0f), Y_AXIS, 0.0, glm::vec3(0.0f, 8.0f, 0.0f));
+	glDrawElements(GL_TRIANGLES, 48, GL_UNSIGNED_SHORT, 0);
+
+
+	glutSwapBuffers(); // Instead of double buffering.
+
+}
+
+// Key down
+void keyDown(unsigned char key, int x, int y)
+{
+	// Orthographic.
+	switch (key)
+	{
+	case 'w':
+		posZ -= 0.1f;
+		break;
+	case 's':
+		posZ += 0.1f;
+		break;
+	case 'a':
+		posX += 0.1f;
+		break;
+	case 'd':
+		posX -= 0.1f;
+		break;
+	case 'r':
+		posY -= 0.1f;
+		break;
+	case 'f':
+		posY += 0.1f;
+		break;
+	}
+
+}
+
+
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+
+int
+main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(768, 768);
+	glutCreateWindow("Liu Beining 101193350 Midterm");
+	glewInit();	//Initializes the glew and prepares the drawing pipeline.
+	init();
+
+
+
+
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutKeyboardFunc(keyDown);
+
+
+	glutMainLoop();
+
+
+
+}
